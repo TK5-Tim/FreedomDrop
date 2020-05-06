c545b8391505adcefd21ed183c414647aa8e349e
faancingfukming <faancingfukming@protonmail.com>
2020-05-06 17:42:45 +0200

    Added Tim's code to BTonly.py

--- Code/BTonly/impexp.py
+++ Code/BTonly/impexp.py
@@ -22,8 +22,11 @@ moved to a more appropriate module
 into Python objects which can then be parsed and handled easier (i.e. JSON objects)
 """
 
+import lib.pcap as pcap
+import cbor2
+import hashlib
 import subprocess
-
+import difflib
 
 def file_len(fname):
     p = subprocess.Popen(['wc', '-l', fname], stdout=subprocess.PIPE,
@@ -32,6 +35,14 @@ def file_len(fname):
     if p.returncode != 0:
         raise IOError(err)
     return int(result.strip().split()[0])
+
+def importPCAP(fname):
+    log = pcap.PCAP(fname)
+    return log
+
+def importInventory(fname):
+    inventory = open(fname)
+    return inventory
 
 def createEntry(rawEntry):
     #return(entryTuple)
@@ -46,7 +57,42 @@ def processEntry(formattedEntry):
 
 def createInventory(inventoryDict):
     #returns an inventory from the inventory dictionary
-    pass
+    log = importPCAP(fname)
+    log.open('r')
+    inventory = open(inventoryDict,'w+')
+    for w in log:
+        e = cbor2.loads(w)
+        href = hashlib.sha256(e[0]).digest()
+        e[0] = cbor2.loads(e[0])
+        # rewrite the packet's byte arrays for pretty printing:
+        e[0] = pcap.base64ify(e[0])
+        fid = e[0][0]
+        seq = e[0][1]
+        inventory.write("%d \n" % seq)
+    log.close()
+    #inventory.close()
+
+def compareInventory(inventoryint, inventoryext):
+    seq_external = set()
+    seq_internal = set()
+    with open(inventoryint) as internal:
+        for line in internal:
+            seq = int(line.rstrip('\n'))
+            seq_internal.add(seq)
+
+    with open(inventoryext) as external:
+        for line in external:
+            seq = int(line.rstrip('\n'))
+            seq_external.add(seq)
+    print(seq_external)
+    print(seq_internal)
+    if seq_internal != seq_external:
+        seq_diff = seq_external - seq_internal
+        print(seq_diff)
+        return seq_diff
+    else:
+        print("both logs are up to date!")
+        return set()
 
 def sendInventory(inventoryDict, socket):
     socket.send(inventoryDict)
@@ -58,9 +104,36 @@ def receivePeerInventory(socket):
     peerInventory = socket.receive()
     return(peerInventory)
 
-def createPayload(fromPeerInventoryDict, inventory):
+def createPayload(fname, inventoryint, inventoryext):
     #how do we create the payload? As a clear text file just like we assume to store them locally?
-    pass
+    log = importPCAP(fname)
+    payload = importPCAP('payload.pcap')
+    seq_payload = compareInventory(inventoryint, inventoryext)
+    if seq_payload == set():
+        return
+    log.open('r')
+    payload.open('a')
+    for w in log:
+        e = cbor2.loads(w)
+        href = hashlib.sha256(e[0]).digest()
+        e[0] = cbor2.loads(e[0])
+        # rewrite the packet's byte arrays for pretty printing:
+        e[0] = pcap.base64ify(e[0])
+        fid = e[0][0]
+        seq = e[0][1]
+        if seq in seq_payload:
+            payload.write(w)
+    payload.close()
+    log.close()
+
+def handlePayload(fname, payload, inventoryDict):
+    log = importPCAP(fname)
+    payload = importPCAP(payload)
+    log.open('a')
+    payload.open('r')
+    for w in payload:
+        log.write(w)
+    createInventory(fname,inventoryDict)
 
 def sendPayload(socket):
     pass
