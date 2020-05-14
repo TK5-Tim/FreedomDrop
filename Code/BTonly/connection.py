64d542f31e1255eb032fd74c3f73de89f5a6af77
Mateusz Palasz <mateusz.palasz@stud.unibas.ch>
2020-05-14 18:27:09 +0200

    back to port_any

--- Code/BTonly/connection.py
+++ Code/BTonly/connection.py
@@ -31,7 +31,7 @@ def establishConnection(isMaster):
     global masterSocket
     global slaveSocket
     uuid = "741f2065-c26a-45fc-b944-6b2bc018a6e8"
-    port = 0
+    #port = 0
 
     # kill any "bluetooth-agent" process that is already running
     subprocess.call("kill -9 `pidof bluetooth-agent`", shell=True)
@@ -87,7 +87,7 @@ def establishConnection(isMaster):
             return (False, slaveSocket, masterSocket)
         print("<Succesfully created a socket>")
         try:
-            slaveSocket.bind(("", port))
+            slaveSocket.bind(("", PORT_ANY))
         except Exception as e:
             print(f"<Failed binding the socket to port {port}>")
             print(e)
